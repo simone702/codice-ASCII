8ed4723816d6efe8fa3c34e95941767e70a60927
Simone <Simone@DESKTOP-N680AHE>
2020-11-10 18:57:15 +0100

    Add project files.

---
+++ ASCIIconvertitore/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ASCIIconvertitore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("inserisci una lettere di cui vuoi sapere in numero in ASCII");
+            char carattere = Convert.ToChar(Console.ReadLine());
+            ushort carattereinASCII = 0;
+            carattereinASCII = carattere;
+            Console.WriteLine(" carattere {0} - carattere in ASCII {1}", carattere, carattereinASCII);
+            Console.ReadKey();
+        }
+    }
+}
